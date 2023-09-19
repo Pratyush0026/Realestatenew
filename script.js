92bad700180d9ae848eee4b018e56b1ec36bc867
Priyanshi Agrawal <131484218+PriyanshiiAgrawall@users.noreply.github.com>
2023-09-19 23:29:03 +0530

    Rent Out Feature
    Added a Rent Out Feature which scrolls down and auto-locates the user.

--- script.js
+++ script.js
@@ -76,3 +76,75 @@ const changeTab = function () {
 }
 
 addEventOnElement(tabBtns, "click", changeTab);
+
+////////////ADDED-FEATURES/////////////////////////////////////
+
+//Scrolling down on clicking Rent-Out//
+
+
+const btnScrollFrom = document.querySelector(".scroll");
+const sectionScrollTo = document.querySelector(".rentout");
+
+btnScrollFrom.addEventListener('click', function (e) {
+  const sectionCoords = sectionScrollTo.getBoundingClientRect();
+
+  window.scrollTo({
+    left: sectionCoords.left + window.pageXOffset, top: sectionCoords.top + window.pageYOffset - 280,
+    behavior: "smooth",
+  })
+})
+
+//Fetching location of the user to input it in location input//
+
+
+
+const inputClick = document.querySelector(".locate");
+const display = function (latitude, longitude) {
+
+  fetch(`https://geocode.xyz/${latitude},${longitude}?geoit=json`)
+    .then(response => {
+      console.log(response);
+      if (!response.ok) {
+        throw new Error(`Fetching Location Taking Time${response.status}`)
+
+      }
+      return response.json()
+    }
+    )
+    .then(data => {
+      console.log(data)
+      console.log(`You are in ${data.city},${data.country} `)
+      inputClick.value = `${data.city},${data.country} `
+    })
+    .catch(err => {
+      console.error(`Something Went Wrong. ${err.message}`)
+    });
+}
+const fetchLocation = function () {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        //console.log(position)
+        const { latitude } = position.coords
+        const { longitude } = position.coords
+
+        // console.log(latitude, longitude);
+        // console.log(`https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu`)
+
+        const coords = [latitude, longitude]
+
+        display(latitude, longitude)
+
+
+      })
+  }
+}
+
+
+
+
+inputClick.addEventListener('click', function () {
+
+  fetchLocation()
+
+})
